fe1a7f3ee23147eb4f9897f90217aff2e4efa77a
Rafał Nowakowski <gee@vp.pl>
2019-07-18 20:21:03 +0200

    Add navigation links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, IndexLink, hashHistory } from 'react-router';
+import style from './App.css';
 
 class App extends Component {
     render() {
         return (
             <Router history={hashHistory}>
-                <Route path='/' component={Home} />
-                <Route path='/contact' component={Contact} />
-            </Router>
-        )
+                <Route path='/' component={Navigation}>
+                    <IndexRoute component={Home} />
+                    <Route path='/contact' component={Contact} />
+                    <Route path='*' component={PageNotFound} />
+                </Route>
+            </Router>        )
     }
 }
 
+const PageNotFound = () => <h1>404 Not Found</h1>;
+const Navigation = props => (
+    <div>
+       <ul>
+            <li><Link to="/" activeStyle={{color: '#48abee'}} onlyActiveOnIndex>Home</Link></li>
+            <li><IndexLink to="/contact" activeClassName='active'>Contact</IndexLink></li>
+        </ul>
+        {props.children}
+    </div>
+);
 const Home = () => <h1>Hej, tu Home component</h1>
 const Contact = () => <h1>A tu Contact component</h1>
 
