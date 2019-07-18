332595e54f5f959e59bcd2830aa45577ab7b58ea
Rafał Nowakowski <gee@vp.pl>
2019-07-18 18:18:58 +0200

    Add first routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path='/' component={Home} />
+                <Route path='/contact' component={Contact} />
+            </Router>
+        )
+    }
 }
+
+const Home = () => <h1>Hej, tu Home component</h1>
+const Contact = () => <h1>A tu Contact component</h1>
 
 export default App;
